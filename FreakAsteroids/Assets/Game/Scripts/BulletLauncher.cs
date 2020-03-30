2ace18af04e179ca7b626a632c28d7d607d0c670
Henrique <ham2@cin.ufpe.br>
2020-03-30 15:27:03 -0300

    Three Bullet Launcher implemented

--- FreakAsteroids/Assets/Game/Scripts/BulletLauncher.cs
+++ FreakAsteroids/Assets/Game/Scripts/BulletLauncher.cs
@@ -6,10 +6,9 @@ using UnityEngine.WSA;
 
 public class BulletLauncher : MonoBehaviour,ILauncher
 {
-    [SerializeField]
-    private Bullet2 bulletPrefab;
+    [SerializeField] protected Bullet2 bulletPrefab;
     
-    public void Launch(ShipWeapon weapon)
+    public virtual void Launch(ShipWeapon weapon)
     {
         var bullet = Instantiate(bulletPrefab,weapon.FirePoint().position,transform.rotation);
         bullet.Launch(weapon.transform.up);
---
+++ FreakAsteroids/Assets/Game/Scripts/ThreeBulletLauncher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ThreeBulletLauncher : BulletLauncher
+{
+    [SerializeField] private float angleBetweenBullets = 45f;
+    public override void Launch(ShipWeapon weapon)
+    {
+        base.Launch(weapon);
+        var rotation = transform.rotation;
+        var weaponDirection = weapon.transform.up;
+        
+
+        var leftBulletDirection =  Quaternion.Euler(0,0,angleBetweenBullets) * weaponDirection;
+        var rotationLeft = Quaternion.Euler(0,0,angleBetweenBullets) * rotation;
+        var bulletLeft = Instantiate(bulletPrefab, weapon.FirePoint().position, rotationLeft);
+        bulletLeft.Launch(leftBulletDirection);
+
+        var rotationRight = Quaternion.Euler(0,0,-angleBetweenBullets) * rotation;
+        var bulletRight = Instantiate(bulletPrefab, weapon.FirePoint().position, rotationRight);
+        var rightBulletDirection =  Quaternion.Euler(0,0,-angleBetweenBullets) * weaponDirection;
+        bulletRight.Launch(rightBulletDirection);
+    }
+}
